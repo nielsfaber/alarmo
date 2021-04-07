a6138a692ea2c535ebfca382ad39eb505ae95fbd
niels <niels@pi.com>
2021-04-07 06:27:36 +0200

    release v1.5.6

--- custom_components/alarmo/card/src/const.ts
+++ custom_components/alarmo/card/src/const.ts
@@ -1,6 +1,6 @@
 
 
-export const CARD_VERSION = "v1.5.5"
+export const CARD_VERSION = "v1.5.6"
 
 export const BUTTONS = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "", "0", "clear"];
 
--- custom_components/alarmo/frontend/src/const.ts
+++ custom_components/alarmo/frontend/src/const.ts
@@ -1,4 +1,4 @@
-export const VERSION = '1.5.5';
+export const VERSION = '1.5.6';
 
 export const platform = 'alarmo';
 export const editConfigService = 'edit_config';
