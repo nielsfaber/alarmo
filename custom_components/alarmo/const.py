195f6450e6674af12fb78468505c4a35e2cd1f31
niels <niels@pi.com>
2021-06-19 07:32:30 +0200

    release v1.6.2

--- custom_components/alarmo/const.py
+++ custom_components/alarmo/const.py
@@ -26,7 +26,7 @@ from homeassistant.components.alarm_control_panel import (
 
 from homeassistant.helpers import config_validation as cv
 
-VERSION = "1.6.1"
+VERSION = "1.6.2"
 NAME = "Alarmo"
 MANUFACTURER = "@nielsfaber"
 
