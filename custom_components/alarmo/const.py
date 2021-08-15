c99deae751846bfbbcbc6003c0a6e1c822b10f19
neliss <niels.r.faber@gmail.com>
2021-08-15 12:39:46 +0200

    release v1.7.1

--- custom_components/alarmo/const.py
+++ custom_components/alarmo/const.py
@@ -26,7 +26,7 @@ from homeassistant.components.alarm_control_panel import (
 
 from homeassistant.helpers import config_validation as cv
 
-VERSION = "1.7.0a"
+VERSION = "1.7.1"
 NAME = "Alarmo"
 MANUFACTURER = "@nielsfaber"
 
--- custom_components/alarmo/frontend/src/const.ts
+++ custom_components/alarmo/frontend/src/const.ts
@@ -1,4 +1,4 @@
-export const VERSION = '1.7.0a';
+export const VERSION = '1.7.1';
 
 export const platform = 'alarmo';
 export const editConfigService = 'edit_config';
