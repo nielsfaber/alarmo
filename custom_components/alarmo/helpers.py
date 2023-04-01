cfcb28c3d1ec268ce22b394d2b3278407f5b7aaa
= <mail@nielsfaber.com>
2023-04-01 07:21:09 +0100

    improve handling of entity without friendly_name in sending notifications

--- custom_components/alarmo/helpers.py
+++ custom_components/alarmo/helpers.py
@@ -4,15 +4,13 @@ from homeassistant.core import (
     HomeAssistant,
 )
 
-from . import const
-
 _LOGGER = logging.getLogger(__name__)
 
 
 def friendly_name_for_entity_id(entity_id: str, hass: HomeAssistant):
     """helper to get friendly name for entity"""
     state = hass.states.get(entity_id)
-    if state and state.attributes["friendly_name"]:
+    if state and state.attributes.get("friendly_name"):
         return state.attributes["friendly_name"]
 
     return entity_id
