f1f7e73033937f24f5b0be1f4d421ff011c166be
= <mail@nielsfaber.com>
2023-10-29 05:32:20 +0000

    release v1.9.11

--- custom_components/alarmo/const.py
+++ custom_components/alarmo/const.py
@@ -21,7 +21,7 @@ from homeassistant.const import (
 from homeassistant.components.alarm_control_panel import AlarmControlPanelEntityFeature
 from homeassistant.helpers import config_validation as cv
 
-VERSION = "1.9.10"
+VERSION = "1.9.11"
 NAME = "Alarmo"
 MANUFACTURER = "@nielsfaber"
 
