ef3ac413af945a4b7d2b92de8711322f8a95d615
nielsfaber <mail@nielsfaber.com>
2024-07-03 16:30:32 +0200

    replace hass.http.register_static_path -> hass.http.async_register_static_paths

--- custom_components/alarmo/panel.py
+++ custom_components/alarmo/panel.py
@@ -3,6 +3,7 @@ import logging
 
 from homeassistant.components import frontend
 from homeassistant.components import panel_custom
+from homeassistant.components.http import StaticPathConfig
 
 from .const import (
     CUSTOM_COMPONENTS,
@@ -24,10 +25,8 @@ async def async_register_panel(hass):
     panel_dir = os.path.join(root_dir, PANEL_FOLDER)
     view_url = os.path.join(panel_dir, PANEL_FILENAME)
 
-    hass.http.register_static_path(
-        PANEL_URL,
-        view_url,
-        cache_headers=False
+    await hass.http.async_register_static_paths(
+        [StaticPathConfig(PANEL_URL, view_url, cache_headers=False)]
     )
 
     await panel_custom.async_register_panel(
