bd255c31b242f672cdd7efc00f072576de0c530a
nielsfaber <mail@nielsfaber.com>
2025-08-10 07:34:35 +0200

    allow setting 'always on' for tamper sensor

--- custom_components/alarmo/frontend/src/views/sensors/sensor-editor-card.ts
+++ custom_components/alarmo/frontend/src/views/sensors/sensor-editor-card.ts
@@ -231,7 +231,7 @@ export class SensorEditorCard extends SubscribeMixin(LitElement) {
           .narrow=${this.narrow}
           header=${localize('panels.sensors.cards.editor.actions.toggle_advanced', this.hass.language)}
         >
-          ${!this.data.type || [ESensorTypes.Environmental, ESensorTypes.Other].includes(this.data.type)
+          ${!this.data.type || [ESensorTypes.Environmental, ESensorTypes.Tamper, ESensorTypes.Other].includes(this.data.type)
         ? html`
                 <alarmo-settings-row .narrow=${this.narrow}>
                   <span slot="heading">
