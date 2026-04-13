nielsfaber/alarmo

commit 2e48a853e6f3cd4c065a4b5bc77d5e4d57689eeb

Unsubscribe Alarmo handlers cleanly on unload

custom_components/alarmo/__init__.py
@@ -520,10 +520,12 @@ class AlarmoCoordinator(DataUpdateCoordinator):
         if self.hass.data[const.DOMAIN]["master"]:
             await self.async_remove_entity("master")
 
-        del self.hass.data[const.DOMAIN]["sensor_handler"]
-        del self.hass.data[const.DOMAIN]["automation_handler"]
-        del self.hass.data[const.DOMAIN]["mqtt_handler"]
-        del self.hass.data[const.DOMAIN]["event_handler"]
+        for key in ["sensor_handler", "automation_handler", "mqtt_handler", "event_handler"]:
+            handler = self.hass.data[const.DOMAIN].get(key)
+            if handler and hasattr(handler, "async_unload"):
+                handler.async_unload()
+            if key in self.hass.data[const.DOMAIN]:
+                del self.hass.data[const.DOMAIN][key]
 
         # remove subscriptions for coordinator
         while len(self._subscriptions):

custom_components/alarmo/alarm_control_panel.py
@@ -108,6 +108,12 @@ async def async_setup_entry(hass, config_entry, async_add_devices):
     @callback
     def async_add_alarm_entity(config: dict):
         """Add each entity as Alarm Control Panel."""
+        if not hass.config_entries.async_get_entry(config_entry.entry_id):
+            _LOGGER.debug(
+                "Skipping area registration for unloaded config entry %s",
+                config_entry.entry_id,
+            )
+            return
         unique_id = _build_unique_id(hass, config["area_id"])
         entity_registry = er.async_get(hass)
         existing_entity_id = entity_registry.async_get_entity_id(
@@ -146,6 +152,12 @@ async def async_setup_entry(hass, config_entry, async_add_devices):
     @callback
     def async_add_alarm_master(config: dict):
         """Add each entity as Alarm Control Panel."""
+        if not hass.config_entries.async_get_entry(config_entry.entry_id):
+            _LOGGER.debug(
+                "Skipping master registration for unloaded config entry %s",
+                config_entry.entry_id,
+            )
+            return
         unique_id = _build_unique_id(hass)
         entity_registry = er.async_get(hass)
         existing_entity_id = entity_registry.async_get_entity_id(
@@ -1310,8 +1322,10 @@ class AlarmoMasterEntity(AlarmoBaseEntity):
                 return
             self.async_update_state()
 
-        async_dispatcher_connect(
-            self.hass, "alarmo_state_updated", async_alarm_state_changed
+        self.async_on_remove(
+            async_dispatcher_connect(
+                self.hass, "alarmo_state_updated", async_alarm_state_changed
+            )
         )
 
         @callback
@@ -1352,7 +1366,9 @@ class AlarmoMasterEntity(AlarmoBaseEntity):
             if event == const.EVENT_READY_TO_ARM_MODES_CHANGED:
                 self.update_ready_to_arm_modes()
 
-        async_dispatcher_connect(self.hass, "alarmo_event", async_handle_event)
+        self.async_on_remove(
+            async_dispatcher_connect(self.hass, "alarmo_event", async_handle_event)
+        )
 
         state = await self.async_get_last_state()
         if state and state.state:

custom_components/alarmo/automations.py
@@ -174,6 +174,10 @@ class AutomationHandler:
 
     def __del__(self):
         """Prepare for removal."""
+        self.async_unload()
+
+    def async_unload(self):
+        """Unload dispatcher subscriptions."""
         while len(self._subscriptions):
             self._subscriptions.pop()()
 

custom_components/alarmo/event.py
@@ -18,6 +18,10 @@ class EventHandler:
 
     def __del__(self):
         """Class destructor."""
+        self.async_unload()
+
+    def async_unload(self):
+        """Unload dispatcher subscription."""
         self._subscription()
 
     @callback

custom_components/alarmo/mqtt.py
@@ -173,6 +173,10 @@ class MqttHandler:
 
     def __del__(self):
         """Prepare for removal."""
+        self.async_unload()
+
+    def async_unload(self):
+        """Unload subscriptions and subscribed topics."""
         while len(self._subscribed_topics):
             self._subscribed_topics.pop()()
         while len(self._subscriptions):

custom_components/alarmo/sensors.py
@@ -195,11 +195,23 @@ class SensorHandler:
 
     def __del__(self):
         """Prepare for removal."""
+        self.async_unload()
+
+    def async_unload(self):
+        """Unload listeners and timers for the sensor handler."""
         if self._state_listener:
             self._state_listener()
             self._state_listener = None
         while len(self._subscriptions):
             self._subscriptions.pop()()
+        while len(self._arm_timers):
+            _key, unsub = self._arm_timers.popitem()
+            if unsub:
+                unsub()
+        while len(self._delay_on_timers):
+            _key, unsub = self._delay_on_timers.popitem()
+            if unsub:
+                unsub()
 
     def async_watch_sensor_states(
         self,

commit ab9dc4f05484a7e10a88d5107c832c03b4580b46

fix(lint): fix linter error introduced by 2e48a853e

custom_components/alarmo/__init__.py
@@ -520,7 +520,12 @@ class AlarmoCoordinator(DataUpdateCoordinator):
         if self.hass.data[const.DOMAIN]["master"]:
             await self.async_remove_entity("master")
 
-        for key in ["sensor_handler", "automation_handler", "mqtt_handler", "event_handler"]:
+        for key in [
+            "sensor_handler",
+            "automation_handler",
+            "mqtt_handler",
+            "event_handler",
+        ]:
             handler = self.hass.data[const.DOMAIN].get(key)
             if handler and hasattr(handler, "async_unload"):
                 handler.async_unload()